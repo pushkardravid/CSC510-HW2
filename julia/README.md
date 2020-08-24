8224549f900f6f820c61a01e27fb31d8bbae5143
ayushjain94 <jain_ayush@outlook.com>
2020-08-23 22:21:58 -0400

    Julia game of life

---
+++ julia/README.md
@@ -0,0 +1,26 @@
+# Game of Life Julia Simulation
+
+### Pre-requisites
+ - Install julia. Instructions can be found [here](https://julialang.org)
+
+### Running the code
+
+ - Clone this repository and navigate to the `\julia` directory
+ - Run the ruby simulation using this command
+
+      ```
+      julia gol.jl
+      ```
+ - The code will ask for the following inputs: 
+      
+     - Number of rows in the grid
+     - Number of columns in the grid
+     - Number of generations to simulate
+   
+These values should be entered as integers
+
+### Output
+
+![Sample output for GOl](https://i.imgur.com/31NyMmx.png)
+
+This represents the state of the grid for 50<sup>th</sup> generation
---
+++ julia/gol.jl
@@ -0,0 +1,70 @@
+function getAliveNeighbours(row, column, arr)
+  rows, columns = size(arr)
+  neighbours = Int64[]
+  if row > 1
+      push!(neighbours, arr[row - 1, column])
+  end
+  if row<rows
+      push!(neighbours, arr[row + 1, column])
+  end
+  if column>1
+      push!(neighbours, arr[row, column - 1])
+  end
+  if column<columns
+      push!(neighbours, arr[row,column + 1])
+  end
+  if row>1 && column>1
+      push!(neighbours, arr[row - 1,column - 1])
+  end
+  if row<rows && column<columns
+      push!(neighbours, arr[row + 1,column + 1])
+  end
+  if row>1 && column<columns
+    push!(neighbours, arr[row - 1,column + 1])
+  end
+  if row<rows && column>1
+    push!(neighbours, arr[row + 1,column - 1])
+  end
+  
+  return count(x->x==1, neighbours)
+
+end
+
+
+function getNextGeneration(currentGeneration)
+  xsize, ysize = size(currentGeneration)
+  newGeneration = zeros(Int8, xsize, ysize)
+  for i = 1:xsize
+    for j = 1:ysize
+      alive = getAliveNeighbours(i,j,currentGeneration)
+      if alive > 0
+          if alive == 2 || alive == 3
+            newGeneration[i,j] = 1
+          end
+      else
+          if alive == 3
+					  newGeneration[i,j] = 1
+				  end
+      end
+    end
+  end
+
+  return newGeneration
+end
+
+
+function main()
+    println("Welcome to game of life. Before we start, please provide few parameters")
+    println("Size of the square matrix: ")
+    size = parse(UInt8, readline())
+    println("How many generations do you want: ")
+    generations = parse(UInt8, readline())
+    initialLife = rand([0, 1], size, size)
+    
+    for i = 1:generations
+        initialLife = getNextGeneration(initialLife)
+        display(initialLife)
+    end
+end
+
+main()
